d2057f80483b7750c415a364fed6d2e202c278b1
hyunyong lee <134678816+hylee-kevin@users.noreply.github.com>
2023-06-07 11:52:54 +0900

    Update runapp.js

--- ch05/runapp.js
+++ ch05/runapp.js
@@ -3,11 +3,11 @@ const http = require('http');
 const server = http.createServer().listen(6060);
 
 server.on('request', (req, res) => {
-    console.log('request arrived.');
+    console.log('Your request arrived.');
     res.write("HostName: " + process.env.HOSTNAME + "\n");
     res.end();
 });
 
 server.on('connection', (socket) => {
-    console.log("Connected");
+    console.log("Your Connected.");
 });
