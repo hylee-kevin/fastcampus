e7202466c13179ef371af0f6b700dab55187a9a6
hyunyong lee <134678816+hylee-kevin@users.noreply.github.com>
2023-06-28 17:22:33 +0900

    Add files via upload

---
+++ ch09/java/my-web-app/src/main/java/com/example/MainApplication.java
@@ -0,0 +1,12 @@
+package com.example.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
+
