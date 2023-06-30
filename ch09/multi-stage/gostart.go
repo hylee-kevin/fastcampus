f24e58f2b8bfb2bdcaee3f31068dd8d722f5d22d
hyunyong lee <134678816+hylee-kevin@users.noreply.github.com>
2023-06-30 10:22:14 +0900

    Update gostart.go

--- ch09/multi-stage/gostart.go
+++ ch09/multi-stage/gostart.go
@@ -31,27 +31,3 @@ func main() {
       http.HandleFunc("/",handler)
       log.Fatal(http.ListenAndServe(":9090",nil))
 }
-
-
-~devteam_go$ vi gostart.go
-package main
-
-import (
-        "fmt"
-        "os"
-        "log"
-        "net/http"
-)
-func handler(w http.ResponseWriter, r *http.Request){
-    name, err := os.Hostname()
-    if err != nil {
-      panic(err)
-    }
-
-    fmt.Fprintln(w, "hostname:", name)
-}
-func main() {
-    fmt.Fprintln(os.Stdout,"Starting Go App. Server......")
-      http.HandleFunc("/",handler)
-      log.Fatal(http.ListenAndServe(":8080",nil))
-}
